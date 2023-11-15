22f1ff2d9ccd69a34a035efcb03ebec09aacc334
Vidhya Soloman <vidsoloman31@gmail.com>
2023-11-15 21:22:50 +0530

    first commit

---
+++ day.py
@@ -0,0 +1,15 @@
+day=int(input("enter the day number"))
+if day==1:
+ print("SUNDAY")
+elif day==2:
+ print("MONDAY")
+elif day==3:
+ print("TUESDAY")
+elif day==4:
+ print("WEDNESDAY")
+elif day==5:
+ print("THURSDAY")
+elif day==6:
+ print("FRIDAY")
+else:
+ print("SATURDAY")
---
+++ swap.py
@@ -0,0 +1,7 @@
+#swap
+a=int(input("enter the 1st number"))
+b=int(input("enter the 2nd number"))
+a=a+b
+b=a-b
+a=a-b
+print(a,b)
