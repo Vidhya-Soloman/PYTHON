633d975f3b937e139ffd3ecf5465e201188d49e8
Vidhya S <147298749+Vidhya-Soloman@users.noreply.github.com>
2023-11-20 22:56:15 +0530

    Add files via upload

---
+++ color.py
@@ -0,0 +1,6 @@
+#CO1 PRGRM 15
+#COLOR IN L1 NOT IN L2
+l1=["red","green","yellow","violet"]
+l2=["blue","red","green"]
+res=set(l1) - set(l2)
+print(res)
---
+++ colors.py
@@ -0,0 +1,5 @@
+#CO1 PRGRM 13
+#PRINTING FIRST AND LAST COLOR
+color=input("Enter colors: ")
+res=color.split(',')
+print(res[0],res[-1])
