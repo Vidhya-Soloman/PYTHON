19ade35f3d05e4f19fb98ce05ab5964ea66be9fd
Vidhya S <147298749+Vidhya-Soloman@users.noreply.github.com>
2023-11-21 00:39:29 +0530

    Update pyramid.py

--- pyramid.py
+++ pyramid.py
@@ -1,8 +1,8 @@
 #CO2 PRGRM3
 #PYRAMID
 def func_pyramid():
- rows = int(input("Enter the number of rows: "))    
- for i in range(1, rows):     
+ n= int(input("Enter the number of rows: "))    
+ for i in range(1,n):     
     for j in range(1, i + 1):     
         print(i*j, end='  ')    
     print()    
